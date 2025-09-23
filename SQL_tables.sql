a4847577452f69c5fadccd0669a8f8c4c57962ac
EduardoRodO <eduardorodriguezortega@outlook.com>
2025-09-23 10:29:10 -0600

    Adding a table called 'book_type_bridge' to link the types of books I can have since I notice I have some books as physical and ebook. Row was deleted from book's table.

--- SQL_tables.sql
+++ SQL_tables.sql
@@ -7,18 +7,16 @@ book_name VARCHAR(100) NOT NULL,
 isbn VARCHAR(50) NOT NULL UNIQUE,
 pages INT NOT NULL,
 publisher_id INT,
-book_type_id INT,
 book_status ENUM('Completed', 'In process', 'Not started', 'Abandoned') NOT NULL,
 personal_thoughts TEXT NULL,
 front_page_url VARCHAR(300),
 book_started VARCHAR(50) NULL,
 book_completed VARCHAR(50) NULL,
 PRIMARY KEY (book_id),
-FOREIGN KEY (publisher_id) REFERENCES publisher(publisher_id),
-FOREIGN KEY (book_type_id) REFERENCES book_type(book_type_id)
+FOREIGN KEY (publisher_id) REFERENCES publisher(publisher_id)
 );
 
-CREATE TABLE book_author(
+CREATE TABLE book_author_bridge(
 book_id INT NOT NULL,
 author_id INT NOT NULL,
 PRIMARY KEY (book_id, author_id),
@@ -44,7 +42,7 @@ genre_name VARCHAR(50) NOT NULL UNIQUE,
 PRIMARY KEY (genre_id)
 );
 
-CREATE TABLE book_genre(
+CREATE TABLE book_genre_bridge(
 book_id INT NOT NULL,
 genre_id INT NOT NULL,
 PRIMARY KEY (book_id, genre_id),
@@ -56,6 +54,14 @@ CREATE TABLE book_type(
 book_type_id INT AUTO_INCREMENT,
 book_type_name VARCHAR(50) NOT NULL,
 PRIMARY KEY (book_type_id)
+);
+
+CREATE TABLE book_type_bridge(
+book_id INT NOT NULL,
+book_type_id INT NOT NULL,
+PRIMARY KEY (book_id, book_type_id),
+FOREIGN KEY (book_id) REFERENCES book(book_id),
+FOREIGN KEY (book_type_id) REFERENCES book_type(book_type_id)
 );
 
 SHOW TABLES;
